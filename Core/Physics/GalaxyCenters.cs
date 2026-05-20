9090b0d6b24205537ba1165dfef246e0afee83c7
Denis <92447229+DocktorCat@users.noreply.github.com>
2026-05-20 14:40:47 +0300

    Delete Core directory

--- Core/Physics/GalaxyCenters.cs
+++
@@ -1,9 +0,0 @@
-﻿using ComputeSharp;
-
-namespace GalaxySim.Core.Physics;
-
-public struct GalaxyCenter
-{
-    public Float4 PositionMass;
-    public Float4 VelocityRadius;
-}
--- Core/Simulation/AsteroidBeltInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GalaxySim.Core.Simulation;
-
-public sealed record AsteroidBeltInfo(
-    float OrbitRenderRadius,
-    float Width,
-    float Density,
-    float Seed);
